bafd130c954acc3486497e1a721c320a0922fa21
Allison Ause <allison.ause@gmail.com>
2022-10-14 15:59:37 -0700

    built out initial architecture and route paths for all pages of Tally-Ho

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import AuthPage from './components/AuthPage/AuthPage';
+import HomePage from './components/HomePage/HomePage';
+import NewTripPage from './components/NewTripPage/NewTripPage';
+import TripPage from './components/TripPage/TripPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/auth/:type" component={AuthPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/new-trip" component={NewTripPage} />
+        <Route path="/trip/:id" component={TripPage} />
+        <Route path="*">
+          <Redirect to="auth/sign-up" />
+        </Route>
+      </Switch>
     </div>
   );
 }
