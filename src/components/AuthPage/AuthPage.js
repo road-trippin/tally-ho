4ecabbd0ad0bc8b07ab4e12c7c20e7adb8f8ca45
Adam Robson <adamray312@gmail.com>
2022-10-16 12:43:10 -0700

    add please sign-in

--- src/components/AuthPage/AuthPage.js
+++ src/components/AuthPage/AuthPage.js
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import {Redirect, useParams, Link } from 'react-router-dom';
 import { authUser } from "../../services/auth";
 
 export default function AuthPage() {
@@ -21,23 +22,20 @@ export default function AuthPage() {
 
   return;
   <>
-    <div className="auth-container">
       <div className="email-wrap">
         <input type="text" className="email" onChange={ (e) => setEmail(e.target.value) } placeholder="email"/>
       </div>
       <div className="password-wrap">
         <input type="text" className="password" onChange={ (e) => setPassword(e.target.value) } placeholder="password" />
       </div>
-
       <div className="button-wrap">
         <button className="auth-button">{ type }</button>
       </div>
-{
-          type === 'sign-in' ?
-            <Link className='auth-link' to='/auth/sign-up'>sign-up</Link> :
-            <Link className='auth-link' to='/auth/sign-in'>sign-in</Link>
-        }
-
-    </div>
+    {
+      type === 'sign-in' ?
+        <Link className='auth-link' to='/auth/sign-up'>sign-up</Link> :
+        <Link className='auth-link' to='/auth/sign-in'>sign-in</Link>
+    }
+    <h1>Please sign-in to continue:</h1>
   </>;
 }
