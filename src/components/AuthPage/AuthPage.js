e59697870b9a77d1d8dc36f3ed71a4d998d62cb2
Adam Robson <adamray312@gmail.com>
2022-10-16 12:35:08 -0700

    add email password inputs and  button

--- src/components/AuthPage/AuthPage.js
+++ src/components/AuthPage/AuthPage.js
@@ -1,3 +1,37 @@
+import { Link } from 'react-router-dom';
+import { authUser } from "../../services/auth";
+
 export default function AuthPage() {
-  return <div></div>;
+  const { type } = useParams();
+
+  const [email, setEmail] = useSatae('');
+  const [password, setPassword] = useState('');
+
+  const authenticateUser = async () => {
+    const userResponse = await authUser(email, password, type);
+    setUser(userResponse);
+    setEmail('');
+    setPassword('');
+    <Redirect to='/' />;
+  };
+
+  if (user) {
+    return <Redirect to='/' />;
+  }
+
+  return;
+  <>
+    <div className="auth-container">
+      <div className="email-wrap">
+        <input type="text" className="email" onChange={ (e) => setEmail(e.target.value) } placeholder="email"/>
+      </div>
+      <div className="password-wrap">
+        <input type="text" className="password" onChange={ (e) => setPassword(e.target.value) } placeholder="password" />
+      </div>
+
+      <div className="button-wrap">
+        <button className="auth-button">{ type }</button>
+      </div>
+    </div>
+  </>;
 }
