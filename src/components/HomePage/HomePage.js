115ceb39111d756c711fdac9c1c66866eb4f1849
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-16 14:03:58 -0700

    Add skeleton of eventual home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,36 @@
+import { Box, Button } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getAllTrips } from '../../services/trips';
 import Header from '../Header/Header';
 
 export default function HomePage() {
-  return <div>
+  const [trips, setTrips] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setLoading(true);
+    getAllTrips()
+      .then(trips => {
+        setTrips(trips);
+        setLoading(false);
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      });
+  }, []);
+
+  return <>
     <Header />
-
-    <span>HomePage</span>
-  </div>;
+    <Button>Create a New Trip</Button>
+    {loading
+      ? <p>loading...</p>
+      : <Box>
+        Your Trips
+        <ul>
+          {trips.map(trip => <li key={trip.id}>{trip.title}</li>)}
+        </ul>
+      </Box>}
+  </>;
 }
