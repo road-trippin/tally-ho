e7f89d6f8e40285088ce3bff214b13b0cb16b734
Allison Ause <allison.ause@gmail.com>
2022-10-17 16:30:11 -0700

    added local state for placeId that is derived from the Autocomplete. Created handler for onPlaceChanged triggered when PlacesAPI is called and a prediction location is chosen. Updated handler for handleAddWayPoint that updates supabase with new waypoints array and setsTrip to rerender page with updated waypoints directions

--- src/components/TripPage/TripPage.js
+++ src/components/TripPage/TripPage.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import MapEmbed from '../MapEmbed/MapEmbed';
 import WaypointList from '../WaypointList/WaypointList';
 import Header from '../Header/Header';
@@ -6,16 +6,37 @@ import { useParams } from 'react-router-dom';
 import { useTrip } from '../../hooks/useTrip';
 import { Autocomplete } from '@react-google-maps/api';
 import { useGoogleScript } from '../../context/GoogleScriptContext';
+import { updateTrip } from '../../services/trips';
 
 export default function TripPage() {
   const { id } = useParams();
-  const { trip } = useTrip(id);
-  const waypointRef = useRef();
+  const { trip, setTrip } = useTrip(id);
+  const [placeId, setPlaceId] = useState('');
+
+  // reference container from Autocomplete
+  const autocompleteRef = useRef();
+
+  const onPlaceChanged = () => {
+    const autocomplete = autocompleteRef.current;
+    if (autocomplete !== null) {
+      const { place_id } = autocomplete.getPlace();
+      setPlaceId(place_id);
+    }
+  };
+
+  const onLoad = (autocomplete) => {
+    autocompleteRef.current = autocomplete;
+  };
 
   const { isLoaded } = useGoogleScript();
 
-  const handleAddWaypoint = () => {
+  const handleAddWaypoint = async (e) => {
+    e.preventDefault();
     // get placeID from waypointRef current value
+    const newWaypoints = [...trip.waypoints, placeId];
+    const updatedTrip = await updateTrip({ ...trip, waypoints: newWaypoints });
+    console.log('updatedTrip', updatedTrip);
+    setTrip(updatedTrip);
     // push placeID to waypoints array in database for current trip
   };
 
@@ -23,18 +44,17 @@ export default function TripPage() {
     <>
       <Header />
       <MapEmbed {...trip} />
-      {trip.waypoints &&
-        <WaypointList waypoints={trip.waypoints}/>
-      }
-      <form>
-        <label htmlFor="waypoint">Add a stop:
-          {isLoaded && 
-            <Autocomplete fields={['place_id']}>
-              <input placeholder="New Stop..." id="waypoint" ref={waypointRef}/>
+      {trip.waypoints && <WaypointList waypoints={trip.waypoints} />}
+      <form onSubmit={handleAddWaypoint}>
+        <label htmlFor="waypoint">
+          Add a stop:
+          {isLoaded && (
+            <Autocomplete fields={['place_id']} onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+              <input placeholder="New Stop..." id="waypoint" />
             </Autocomplete>
-          }
+          )}
         </label>
-        <button onClick={handleAddWaypoint}>+</button>
+        <button>+</button>
       </form>
     </>
   );
--- src/services/trips.js
+++ src/services/trips.js
@@ -1,49 +1,31 @@
 import { checkError, client } from './client';
 
 export async function getAllTrips() {
-  const response = await client
-    .from('trips')
-    .select()
-    .order('created_at', { ascending: false });
-  
+  const response = await client.from('trips').select().order('created_at', { ascending: false });
+
   return checkError(response);
 }
 
 export async function getTripById(id) {
-  const response = await client
-    .from('trips')
-    .select()
-    .match({ id })
-    .single();
+  const response = await client.from('trips').select().match({ id }).single();
 
   return checkError(response);
 }
 
 export async function createTrip(tripData) {
-  const response = await client
-    .from('trips')
-    .insert(tripData)
-    .single();
-  
+  const response = await client.from('trips').insert(tripData).single();
+
   return checkError(response);
 }
 
-export async function updateTrip(id, tripData) {
-  const response = await client
-    .from('trips')
-    .update(tripData)
-    .match({ id })
-    .single();
-  
+export async function updateTrip(tripData) {
+  const response = await client.from('trips').update(tripData).match({ id: tripData.id }).single();
+
   return checkError(response);
 }
 
 export async function deleteTrip(id) {
-  const response = await client
-    .from('trips')
-    .delete()
-    .match({ id })
-    .single();
+  const response = await client.from('trips').delete().match({ id }).single();
 
   return checkError(response);
 }
