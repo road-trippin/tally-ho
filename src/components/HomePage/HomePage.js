c5f97a56f369978fdba58dd71f8953cc06f73803
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-17 14:29:53 -0700

    HomePage: Add edit buttons to trip list

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,20 +1,44 @@
-import { Box, Button } from '@chakra-ui/react';
 import Header from '../Header/Header';
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, Text } from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
 import useTrips from '../../hooks/useTrips';
 
 export default function HomePage() {
   const { trips, loading } = useTrips();
+  const history = useHistory();
+
+  const handleEditTrip = (id) => {
+    history.push(`/trip/${id}`);
+  };
 
   return <>
     <Header />
-    <Button>Create a New Trip</Button>
+    <Button marginY="20px">Create a New Trip</Button>
     {loading
       ? <p>loading...</p>
-      : <Box>
-        Your Trips
-        <ul>
-          {trips.map(trip => <li key={trip.id}>{trip.title}</li>)}
-        </ul>
+      : <Box
+        maxWidth="500px"
+        marginX="auto"
+        boxShadow="lg"
+        rounded="lg"
+        padding="10px"
+      >
+        <Text textAlign="left" fontSize="1.5rem">Your Trips</Text>
+        <Accordion>
+          {trips.map(trip => (
+            <AccordionItem key={trip.id}>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  {trip.title}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+              <AccordionPanel padding="10px">
+                <Button onClick={() => handleEditTrip(trip.id)}>Edit</Button>
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </Box>}
   </>;
 }
