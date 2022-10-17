34005fe7614d5d853748fffc6177ed366b973ae8
Allison Ause <allison.ause@gmail.com>
2022-10-17 10:53:30 -0700

    connected to google maps API and put in GoogleMap componentwith default center property

--- src/components/MapEmbed/MapEmbed.js
+++ src/components/MapEmbed/MapEmbed.js
@@ -1,5 +1,27 @@
+import { SkeletonText } from '@chakra-ui/react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { useState } from 'react';
+
 export default function MapEmbed() {
+  // const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+
+  const center = { lat: 36.0544, lng: -112.1401 };
+
+  // loads Google script
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) {
+    return <SkeletonText />;
+  }
+
   return (
-    <iframe width="700px" height="500px"/>
+    <GoogleMap
+      center={center}
+      zoom={15}
+      mapContainerStyle={{ width: '500px', height: '500px' }}
+      // onLoad={(map) => setMap(map)}
+    ></GoogleMap>
   );
 }
