3bdead8f8bfaf81793b4b4962c08c6aba4500598
Adam Robson <adamray312@gmail.com>
2022-10-18 10:51:06 -0700

    wrap inputs in Autocomplete tags

--- src/components/NewTripPage/NewTripPage.js
+++ src/components/NewTripPage/NewTripPage.js
@@ -3,6 +3,7 @@ import './NewTripPage.css';
 import Header from '../Header/Header';
 import { useUserContext } from '../../context/UserContext';
 import { useGoogleScript } from '../../context/GoogleScriptContext';
+import { Autocomplete } from '@react-google-maps/api';
 
 export default function NewTripPage() {
 
@@ -14,13 +15,13 @@ export default function NewTripPage() {
     return <SkeletonText />;
   }
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
+  const handleSubmit = (e) => {
+    // e.preventDefault();
 
   //   functionality for processing New Trip Form info
   //   FormData.get
   //   setTrip
-  // };
+  };
 
   return (
     <div>
@@ -37,21 +38,27 @@ export default function NewTripPage() {
       >
         <h2>Start a New Trip!</h2>
         <form className="new-trip-form">
-          <InputGroup display="flex" flex-direction="column">
-            <label>
+          {isLoaded && (
+            <InputGroup display="flex" flex-direction="column">
+              <label>
               Trip Name:
-              <Input variant="flushed" placeholder="Best Trip Ever!" />
-            </label>
-            <label>
+                <Input variant="flushed" placeholder="Best Trip Ever!" />
+              </label>
+              <label>
               Origin:
-              <Input variant="flushed" placeholder="Rome, Illinois" />
-            </label>
-            <label>
-              Destination:
-              <Input variant="flushed" placeholder="Paris, Texas" />
-            </label>
-          </InputGroup>
-          <Button onSubmit={handleSubmit}>Embark!</Button>
+                <Autocomplete>
+                  <Input variant="flushed" placeholder="Rome, Illinois" />
+                </Autocomplete>
+              </label>
+              <label>
+                Destination:
+                <Autocomplete>
+                  <Input variant="flushed" placeholder="Paris, Texas" />
+                </Autocomplete>
+              </label>
+            </InputGroup>
+          )}
+          <Button onSubmit={ handleSubmit }>Embark!</Button>
         </form>
       </Box>
     </div>
