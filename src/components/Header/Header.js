00057ba9d68a3cd29dad6b899929308ca0b9aa5b
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-19 16:05:02 -0700

    Lay out header with chakra and introduce and hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,7 @@
+import { Box, Flex, Menu, Text } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useUserContext } from '../../context/UserContext';
 import { signOut } from '../../services/auth';
 import './Header.css';
@@ -13,13 +16,22 @@ export default function Header() {
 
   return (
     <header>
-      <h2><a href="/" className="logo-link">Tally Ho!</a></h2>
-      { user && (
-        <div>
-          <p id="hello-message">Hello {user.email}</p>
-          <button onClick={handleSignOut}>Sign Out</button>
-        </div>
-      )}
+      <Flex
+        padding="10px"
+        backgroundColor="#319795"
+        justifyContent="space-between"
+        alignItems="center"
+        color="white"
+      >
+        <Link to="/">
+          <Text fontSize="3xl" fontWeight="bold">Tally Ho!</Text>
+        </Link>
+        { user && (
+          <Menu>
+            <HamburgerIcon w={8} h={8}></HamburgerIcon>
+          </Menu>
+        )}
+      </Flex>
     </header>
   );
 }
