6be921c41d80e6b2a31ecfd4a68700f8f1f6df2e
Dillon Brock <mdillon1297@gmail.com>
2022-10-19 12:01:50 -0700

    moved trip notes into modal

--- src/components/TripNotes/TripNotes.js
+++ src/components/TripNotes/TripNotes.js
@@ -1,10 +1,11 @@
-import { Box, Button, Textarea } from '@chakra-ui/react';
+import { Box, Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Textarea, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
 import { updateTrip } from '../../services/trips';
 
 export default function TripNotes({ trip, setTrip }) {
   const [isEditing, setIsEditing] = useState(false);
   const [tripNotes, setTripNotes] = useState('');
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleEdit = () => {
     setIsEditing(true);
@@ -17,13 +18,28 @@ export default function TripNotes({ trip, setTrip }) {
   };
 
   return (
-    <Box>
-      <Textarea
-        isDisabled={!isEditing}
-        defaultValue={trip.notes}
-        onChange={(e) => setTripNotes(e.target.value)}
-      />
-      <Button onClick={isEditing ? handleSave : handleEdit}>{isEditing ? 'Save' : 'Edit'}</Button>
-    </Box>
+    <>
+      <Button onClick={onOpen}>{trip.notes ? 'View Trip Notes' : 'Add Trip Notes'}</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Notes for {trip.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box h="100px">
+              {isEditing ?
+                <Textarea
+                  defaultValue={trip.notes}
+                  onChange={(e) => setTripNotes(e.target.value)}
+                />
+                : <p>{trip.notes}</p>
+              }
+            </Box>
+            <Button onClick={isEditing ? handleSave : handleEdit}>{isEditing ? 'Save' : 'Edit'}</Button>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
