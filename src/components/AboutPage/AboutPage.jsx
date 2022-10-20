092cf2717a13d27d8d19cbe7584acadf2e7b498b
Adam Robson <adamray312@gmail.com>
2022-10-19 17:57:38 -0700

    add the map and link around first and last names and start the icons

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -24,28 +24,57 @@ const AboutPage = () => {
       linked: 'adamrrobson'
     }];
 
-  const photoLink = (i) => {
-    const localLink = `process.env.PUBLIC_URL/assets/${i}.png`;
-    return localLink;
-  };
+    const localLink = (dev) => {
+      const link = `process.env.PUBLIC_URL/assets/${dev}.png`;
+      return link;
+    };
 
-  const gitPhoto = (devGit) => {
-    const gitLink = `https://github.com/${devGit}.png`;
-    return gitLink;
-  };
+    const gitLink = (dev) => {
+      const link = `url(www.github.com/${dev})`
+      return link;
+    };
 
-  const linkedInPhoto = (devLinkedIn) => {
-    const linkedLink = `https://www.linkedin.com/in/${devLinkedIn}`;
-    return linkedLink;
-  };
+    const linkedLink = (dev) => {
+      const link = `url(www.linkedin.com/in/${dev})`
+    }
+
 
   return (
-    <section className="wrapper">
-      <div className="mission">
-        <h2 className="title">TALLY-HO!</h2>
-        <h4 className="mission">......</h4>
-        <strong>Don&apos;t forget your trips!</strong>
-      </div>
+    <section>
+
+      <h2 className="title">TALLY-HO!</h2>
+      <h4 className="mission">......</h4>
+      <strong>Don&apos;t forget your trips!</strong>
+
+      {
+        devs.map((dev) => {
+          <div>
+            <a href={ devs.map((dev) => gitPhoto(dev.git)) } target="_blank">
+              {
+                photoLink(dev.local)
+              }</a><br />
+            <a href={ devs.map((dev) => gitPhoto(dev.git)) } target="_blank">
+              {
+                devs.map((dev) => dev.local)
+              }</a><br />
+            <a href={ devs.map((dev) => linkedLink(dev.linked))}
+
+
+            }></a>
+          </div>
+        })
+      }
+
+    </section>
+   )
+
+
+
+
+}
+
+
+
       { devs.map((dev) => {
         <div>
           <a href={ devs.map(
