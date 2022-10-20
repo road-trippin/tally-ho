47ab8c3165c012ff2ba51f9bfb7c9d6a99fc49fa
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-20 13:35:11 -0700

    Add home navigation link on about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 export default function AboutPage() {
   const devs = [
@@ -26,28 +27,33 @@ export default function AboutPage() {
   }
 
   return (
-    <section>
-      <h2 className="title">TALLY-HO!</h2>
-      <h4 className="subtitle">ABOUT US</h4>
-      {
-        devs.map((dev, index) => (
-          <div key={ index }>
-            <a href={ gitHubFn(dev) } target="_blank noreferrer">
-              <img src={ photoFn(dev) } />
-              <p>{ dev.name }</p>
-              <p>this is for something about me</p>
-            </a>
+    <>
+      <Header
+        navLinks={[{ text: 'Home', path: '/' }]}
+      />
+      <section>
+        <h2 className="title">TALLY-HO!</h2>
+        <h4 className="subtitle">ABOUT US</h4>
+        {
+          devs.map((dev, index) => (
+            <div key={ index }>
+              <a href={ gitHubFn(dev) } target="_blank noreferrer">
+                <img src={ photoFn(dev) } />
+                <p>{ dev.name }</p>
+                <p>this is for something about me</p>
+              </a>
 
-            <a href={ gitHubFn(dev) } target="_blank noreferrer">
-              <img width={ '32'} src={ iconFn('gitHubIcon') } alt="gitHub-icon" />
-            </a>
+              <a href={ gitHubFn(dev) } target="_blank noreferrer">
+                <img width={ '32'} src={ iconFn('gitHubIcon') } alt="gitHub-icon" />
+              </a>
 
-            <a href={ linkedInFn(dev) } target="_blank noreferrer">
-              <img width={ '32'} src={ iconFn('linkedInIcon') } alt="linkedIn-icon" />
-            </a>
-          </div>
-        ))
-      }
-    </section>
+              <a href={ linkedInFn(dev) } target="_blank noreferrer">
+                <img width={ '32'} src={ iconFn('linkedInIcon') } alt="linkedIn-icon" />
+              </a>
+            </div>
+          ))
+        }
+      </section>
+    </>
   );
 }
