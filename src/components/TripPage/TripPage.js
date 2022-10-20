cde802ef19fb589ff10b8091162d2403412571a5
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-19 19:25:08 -0700

    Use DirectionsService directly in MapEmbed
    The callback based components that @react-google-maps/api provides look
    really clean and declaritive, but it's a bit of a nightmare to control
    api requests through rerenders. This imperative approach gives more
    control.
    
    This patch also decouples MapEmbed from its parent a bit by using a
    generic callback prop instead of passing a state setter directly down. I
    think exposing the full route makes more sense for this component.

--- src/components/TripPage/TripPage.js
+++ src/components/TripPage/TripPage.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import MapEmbed from '../MapEmbed/MapEmbed';
 import Header from '../Header/Header';
 import { useParams } from 'react-router-dom';
@@ -11,11 +11,13 @@ export default function TripPage() {
   const { trip, setTrip } = useTrip(id);
   const [legs, setLegs] = useState(null);
 
+  const onRouteChanged = useCallback((route) => setLegs(route.legs), [setLegs]);
+
   return (
     <>
       <Header />
       <Box pos="relative">
-        <MapEmbed {...trip} setLegs={setLegs} />
+        <MapEmbed waypoints={trip.waypoints} onRouteChanged={onRouteChanged} />
         <SidePanel trip={trip} setTrip={setTrip} legs={legs} />
       </Box>
     </>
