cd8c13aea05f77483be5b4c6b981f9aa13513fe3
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-20 14:05:39 -0700

    Make bio page header text consistent

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -35,9 +35,8 @@ export default function AboutPage() {
         backgroundSize="cover"
       >
         <Text
+          className="brand title"
           textAlign="center"
-          fontSize="5xl"
-          fontWeight="light"
           marginY="40px"
         >
           About Us
